a6f38578d06707d896ecc0ceec1c6cfa93b7d153
Khalil Claybon <kclaybon1@gmail.com>
2018-06-27 01:10:44 -0400

    remove reverse proxy stuff

--- compose-network/badgeforce/apache2-proxy/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-AWS_REGION=us-east-1 \
-AWS_ACCESS_KEY_ID=AKIAJ2LAZS3LSXH3C6EQ \
-AWS_SECRET_ACCESS_KEY=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 \
-lego -a --email="engineering@badgeforce.io" --domains="testnet.badgeforce.io" --dns="route53" --dns-timeout=60 run
-# certbot certonly --agree-tos --dns-route53 --email engineering@badgeforce.io -d testnet.badgeforce.io
